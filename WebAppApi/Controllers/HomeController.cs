d0b524b5e456455c05115701e225468409849a6f
RahulDeoRanjan <onlyrdr@gmail.com>
2023-11-19 16:51:51 +0530

    initial code

---
+++ WebAppApi/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppApi.Controllers
+{
+    //testClass
+    //[ApiController]
+    //[Route("[controller]")]
+    //public class HomeController : Controller
+    //{
+    //    public IActionResult Index()
+    //    {
+    //        return View();
+    //    }
+    //}
+}
